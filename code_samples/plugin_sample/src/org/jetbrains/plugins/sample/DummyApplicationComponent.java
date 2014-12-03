541e34321e69845406f3e2675d3778b8588992af
Anna Bulenkova <anna.bulenkova@jetbrains.com>
2014-12-03 16:43:01 +0100

    Dummy plugin stub to illustrate plugin.xml

---
+++ code_samples/plugin_sample/src/org/jetbrains/plugins/sample/DummyApplicationComponent.java
@@ -0,0 +1,9 @@
+package org.jetbrains.plugins.sample;
+
+import com.intellij.openapi.components.ApplicationComponent;
+
+/**
+ * @author Anna Bulenkova
+ */
+interface DummyApplicationComponent extends ApplicationComponent{
+}
---
+++ code_samples/plugin_sample/src/org/jetbrains/plugins/sample/DummyModuleComponent.java
@@ -0,0 +1,9 @@
+package org.jetbrains.plugins.sample;
+
+import com.intellij.openapi.module.ModuleComponent;
+
+/**
+ * @author Anna Bulenkova
+ */
+public interface DummyModuleComponent extends ModuleComponent{
+}

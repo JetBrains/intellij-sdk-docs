d22ef350b89ce424d67dd14bb892970ddb24e978
Anna Bulenkova <anna.bulenkova@jetbrains.com>
2014-12-04 15:14:18 +0100

    Cleanup

--- code_samples/plugin_sample/src/org/jetbrains/plugins/sample/SimpleAction.java
+++ code_samples/plugin_sample/src/org/jetbrains/plugins/sample/SimpleAction.java
@@ -2,13 +2,14 @@ package org.jetbrains.plugins.sample;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Anna Bulenkova
  */
 public class SimpleAction extends AnAction {
     @Override
-    public void actionPerformed(AnActionEvent anActionEvent) {
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
 
     }
 }

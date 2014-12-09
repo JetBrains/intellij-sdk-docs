7be7b9a0fb471cea7be8b5b26ccae2034166fd4d
Anna Bulenkova <anna.bulenkova@jetbrains.com>
2014-12-09 16:59:13 +0100

    [code] LandDataKeys changes to CommonDataKeys

--- code_samples/editor_basics/src/org/jetbrains/plugins/editor/basics/EditorIllustration.java
+++ code_samples/editor_basics/src/org/jetbrains/plugins/editor/basics/EditorIllustration.java
@@ -2,7 +2,7 @@ package org.jetbrains.plugins.editor.basics;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 
@@ -16,8 +16,8 @@ public class EditorIllustration extends AnAction {
     }
     @Override
     public void update(AnActionEvent e) {
-        final Project project = e.getData(LangDataKeys.PROJECT);
-        final Editor editor = e.getData(LangDataKeys.EDITOR);
+        final Project project = e.getData(CommonDataKeys.PROJECT);
+        final Editor editor = e.getData(CommonDataKeys.EDITOR);
         e.getPresentation().setVisible((project != null && editor != null && editor.getSelectionModel().hasSelection()));
     }
 }

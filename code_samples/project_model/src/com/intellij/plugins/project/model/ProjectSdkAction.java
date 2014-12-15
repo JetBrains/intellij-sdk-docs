6df657389ad4f018cf93cbde728761707e662f1f
Anna Bulenkova <anna.bulenkova@jetbrains.com>
2014-12-15 15:25:32 +0100

    [code] Project sdk sample

---
+++ code_samples/project_model/src/com/intellij/plugins/project/model/ProjectSdkAction.java
@@ -0,0 +1,33 @@
+package com.intellij.plugins.project.model;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.ui.Messages;
+
+/**
+ * @author Anna Bulenkova
+ */
+public class ProjectSdkAction extends AnAction {
+    @Override
+    public void actionPerformed(final AnActionEvent e) {
+        Project project = e.getProject();
+        if (project != null) {
+            String projectSDKName = ProjectRootManager.getInstance(project).getProjectSdkName();
+            String newProjectSdkName = "New Sdk Name";
+            ProjectRootManager.getInstance(project).setProjectSdkName(newProjectSdkName);
+            Messages.showInfoMessage(projectSDKName + " has changed to " + newProjectSdkName, "Project Sdk Info");
+        }
+    }
+
+    @Override
+    public void update(final AnActionEvent e) {
+        Project project = e.getProject();
+        if (project != null) {
+            Sdk sdk = ProjectRootManager.getInstance(project).getProjectSdk();
+            e.getPresentation().setEnabledAndVisible(sdk != null);
+        }
+    }
+}

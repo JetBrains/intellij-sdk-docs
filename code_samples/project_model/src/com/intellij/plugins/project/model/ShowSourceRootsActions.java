4d73634b0211f0b6fb242db54ff84748d9e220c9
Anna Bulenkova <anna.bulenkova@jetbrains.com>
2014-12-15 12:55:26 +0100

    [code] Project roots illustration

---
+++ code_samples/project_model/src/com/intellij/plugins/project/model/ShowSourceRootsActions.java
@@ -0,0 +1,33 @@
+package com.intellij.plugins.project.model;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+
+/**
+ * @author Anna Bulenkova
+ */
+public class ShowSourceRootsActions extends AnAction {
+    @Override
+    public void actionPerformed(final AnActionEvent anActionEvent) {
+        Project project = anActionEvent.getProject();
+        if (project == null) return;
+        String projectName = project.getName();
+        StringBuilder sourceRootsList = new StringBuilder();
+        VirtualFile[] vFiles = ProjectRootManager.getInstance(project).getContentSourceRoots();
+        for (VirtualFile file : vFiles) {
+            sourceRootsList.append(file.getUrl()).append("\n");
+        }
+        Messages.showInfoMessage("Source roots for the " + projectName + " plugin:\n" + sourceRootsList, "Project Properties");
+    }
+
+    @Override
+    public void update(final AnActionEvent e) {
+        boolean visibility = e.getProject() != null;
+        e.getPresentation().setEnabled(visibility);
+        e.getPresentation().setVisible(visibility);
+    }
+}

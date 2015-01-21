3be01aaccfe1bca6d438aff98e53f6c5cf8daef8
Anna Bulenkova <anna.bulenkova@jetbrains.com>
2015-01-21 12:25:55 +0100

    [code] action.actionPerformed rewritten

--- code_samples/register_actions/src/org/jetbrains/tutorials/actions/SimpleAction.java
+++ code_samples/register_actions/src/org/jetbrains/tutorials/actions/SimpleAction.java
@@ -1,7 +1,10 @@
 package org.jetbrains.tutorials.actions;
 
-import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 
 /**
  * @author Anna Bulenkova
@@ -9,7 +12,10 @@ import com.intellij.openapi.project.Project;
 public class SimpleAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
-
+        Object navigatable = anActionEvent.getData(CommonDataKeys.NAVIGATABLE);
+        if (navigatable != null) {
+            Messages.showDialog(navigatable.toString(), "Selected Element:", new String[]{"OK"}, -1, null);
+        }
     }
 
     @Override

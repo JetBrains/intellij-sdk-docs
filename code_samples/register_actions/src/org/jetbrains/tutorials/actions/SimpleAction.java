debcb42f472f367210ca84005fe7f2998be63ad6
Anna Bulenkova <anna.bulenkova@jetbrains.com>
2015-01-21 11:20:09 +0100

    [code] action.update() sample

--- code_samples/register_actions/src/org/jetbrains/tutorials/actions/SimpleAction.java
+++ code_samples/register_actions/src/org/jetbrains/tutorials/actions/SimpleAction.java
@@ -1,7 +1,7 @@
 package org.jetbrains.tutorials.actions;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.project.Project;
 
 /**
  * @author Anna Bulenkova
@@ -10,5 +10,14 @@ public class SimpleAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
 
+    }
+
+    @Override
+    public void update(AnActionEvent anActionEvent) {
+        final Project project = anActionEvent.getData(CommonDataKeys.PROJECT);
+        if (project != null)
+            return;
+        Object navigatable = anActionEvent.getData(CommonDataKeys.NAVIGATABLE);
+        anActionEvent.getPresentation().setVisible(navigatable != null);
     }
 }

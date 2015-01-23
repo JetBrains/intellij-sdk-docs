f55db355015d1eebccc27cf088610d6000a6ab08
Anna Bulenkova <anna.bulenkova@jetbrains.com>
2015-01-23 12:31:16 +0100

    [code] extending DefaultActionGroup sample

---
+++ code_samples/register_actions/src/org/jetbrains/tutorials/actions/CustomDefaultActionGroup.java
@@ -0,0 +1,20 @@
+package org.jetbrains.tutorials.actions;
+
+import com.intellij.icons.AllIcons;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.editor.Editor;
+
+/**
+ * @author Anna Bulenkova
+ */
+public class CustomDefaultActionGroup extends DefaultActionGroup {
+    @Override
+    public void update(AnActionEvent event) {
+        Editor editor = event.getData(CommonDataKeys.EDITOR);
+        event.getPresentation().setVisible(true);
+        event.getPresentation().setEnabled(editor != null);
+        event.getPresentation().setIcon(AllIcons.General.Error);
+    }
+}
---
+++ code_samples/register_actions/src/org/jetbrains/tutorials/actions/CustomGroupedAction.java
@@ -0,0 +1,14 @@
+package org.jetbrains.tutorials.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * @author Anna Bulenkova
+ */
+public class CustomGroupedAction extends AnAction {
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+        //Does nothing
+    }
+}

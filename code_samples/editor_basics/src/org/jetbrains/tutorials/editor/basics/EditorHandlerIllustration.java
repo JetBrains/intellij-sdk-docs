d904d6e34cd6c2e9c05154212fac42374e3e75ae
Anna Bulenkova <anna.bulenkova@jetbrains.com>
2015-01-26 15:31:22 +0100

    [code] cleanup - annotations, naming

--- code_samples/editor_basics/src/org/jetbrains/tutorials/editor/basics/EditorHandlerIllustration.java
+++ code_samples/editor_basics/src/org/jetbrains/tutorials/editor/basics/EditorHandlerIllustration.java
@@ -8,13 +8,14 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
 import com.intellij.openapi.editor.actionSystem.EditorActionManager;
 import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Anna Bulenkova
  */
 public class EditorHandlerIllustration extends AnAction {
     @Override
-    public void actionPerformed(AnActionEvent anActionEvent) {
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
         final Editor editor = anActionEvent.getRequiredData(CommonDataKeys.EDITOR);
         EditorActionManager actionManager = EditorActionManager.getInstance();
         //Insert one more caret below the active caret
@@ -22,10 +23,10 @@ public class EditorHandlerIllustration extends AnAction {
         actionHandler.execute(editor, editor.getCaretModel().getCurrentCaret(), anActionEvent.getDataContext());
     }
     @Override
-    public void update(final AnActionEvent e) {
+    public void update(@NotNull final AnActionEvent anActionEvent) {
         //Set visible if at least one caret is available
-        final Project project = e.getData(CommonDataKeys.PROJECT);
-        final Editor editor = e.getData(CommonDataKeys.EDITOR);
-        e.getPresentation().setVisible((project != null && editor != null && !editor.getCaretModel().getAllCarets().isEmpty()));
+        final Project project = anActionEvent.getData(CommonDataKeys.PROJECT);
+        final Editor editor = anActionEvent.getData(CommonDataKeys.EDITOR);
+        anActionEvent.getPresentation().setVisible((project != null && editor != null && !editor.getCaretModel().getAllCarets().isEmpty()));
     }
 }

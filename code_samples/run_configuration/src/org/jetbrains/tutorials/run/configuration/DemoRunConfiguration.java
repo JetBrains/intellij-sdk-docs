93eb0590c067775213e939a3c14b876a313b8e99
Anna Bulenkova <anna.bulenkova@jetbrains.com>
2015-02-04 12:43:55 +0100

    [code] simple custom run configuration

---
+++ code_samples/run_configuration/src/org/jetbrains/tutorials/run/configuration/DemoRunConfiguration.java
@@ -0,0 +1,36 @@
+package org.jetbrains.tutorials.run.configuration;
+
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.Executor;
+import com.intellij.execution.configurations.*;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.openapi.options.SettingsEditor;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @author Anna Bulenkova
+ */
+public class DemoRunConfiguration extends RunConfigurationBase {
+    protected DemoRunConfiguration(Project project, ConfigurationFactory factory, String name) {
+        super(project, factory, name);
+    }
+
+    @NotNull
+    @Override
+    public SettingsEditor<? extends RunConfiguration> getConfigurationEditor() {
+        return new DemoSettingsEditor();
+    }
+
+    @Override
+    public void checkConfiguration() throws RuntimeConfigurationException {
+
+    }
+
+    @Nullable
+    @Override
+    public RunProfileState getState(@NotNull Executor executor, @NotNull ExecutionEnvironment executionEnvironment) throws ExecutionException {
+        return null;
+    }
+}
---
+++ code_samples/run_configuration/src/org/jetbrains/tutorials/run/configuration/DemoRunConfigurationType.java
@@ -0,0 +1,39 @@
+package org.jetbrains.tutorials.run.configuration;
+
+import com.intellij.execution.configurations.ConfigurationFactory;
+import com.intellij.execution.configurations.ConfigurationType;
+import com.intellij.icons.AllIcons;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * @author Anna Bulenkova
+ */
+public class DemoRunConfigurationType implements ConfigurationType {
+    @Override
+    public String getDisplayName() {
+        return "Demo";
+    }
+
+    @Override
+    public String getConfigurationTypeDescription() {
+        return "Demo Run Configuration Type";
+    }
+
+    @Override
+    public Icon getIcon() {
+        return AllIcons.General.Information;
+    }
+
+    @NotNull
+    @Override
+    public String getId() {
+        return "DEMO_RUN_CONFIGURATION";
+    }
+
+    @Override
+    public ConfigurationFactory[] getConfigurationFactories() {
+        return new ConfigurationFactory[]{new DemoConfigurationFactory(this)};
+    }
+}

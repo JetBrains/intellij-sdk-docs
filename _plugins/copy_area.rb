b6d18f2bba314d69cbb2ca9e128b59f4b277fec0
kisenka <qtuzov@gmail.com>
2015-06-02 13:02:35 +0300

    IJSDK-32 "Copy link to source to clipboard" button

---
+++ _plugins/copy_area.rb
@@ -0,0 +1,20 @@
+class CopyToClipboard < Liquid::Tag
+  def initialize(tagName, markup, tokens)
+    super
+
+    @text = markup
+  end
+
+  def render(context)
+    text = @text.strip.gsub(/^'|"/, '').gsub(/'|"$/, '')
+
+    <<-HTML
+    <div class="copy-area">
+      <input class="copy-area__field" value="#{text}">
+      <button class="btn copy-area__button" data-clipboard-text="#{text}">Copy</button>
+    </div>
+    HTML
+  end
+
+  Liquid::Template.register_tag "copyArea", self
+end

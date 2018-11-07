35a9c9bffc9a0d31a700572c3a4bf582c0f56b0d
Shawn Presser <shawnpresser@gmail.com>
2018-11-07 13:29:51 -0600

    Fix AnnotatorTestData

--- code_samples/simple_language_plugin/testData/AnnotatorTestData.java
+++ code_samples/simple_language_plugin/testData/AnnotatorTestData.java
@@ -2,6 +2,6 @@ public class Test {
   public static void main(String[] args) {
     System.out.println("simple:website");
     System.out.println("simple:key with spaces");
-    System.out.println("simple:<error descr="Unresolved property">websit"</error>);
+    System.out.println("simple:<error descr=\"Unresolved property\">websit</error>");
   }
 }

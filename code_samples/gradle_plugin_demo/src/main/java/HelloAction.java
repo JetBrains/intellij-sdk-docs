9203bada0b886a89c06b0741d26fb9fdc84528e9
JohnHake <john.hake@jetbrains.com>
2019-03-08 11:38:24 -0800

    Revert, use a code sample other than gradle_plugin_demo as pathfinder
    This reverts commit 544f6dcd239eb3d29fcb004cdc043d3a03d2b95c.

--- code_samples/gradle_plugin_demo/src/main/java/HelloAction.java
+++ code_samples/gradle_plugin_demo/src/main/java/HelloAction.java
@@ -1,7 +1,3 @@
-/*
- * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
- */
-
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;

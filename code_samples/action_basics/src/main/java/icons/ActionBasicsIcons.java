73f214510a543764d2cba20582323d816b87121e
JohnHake <john.hake@jetbrains.com>
2019-06-03 17:39:30 -0700

    Incorporate Nik's action_basics feedback.

--- code_samples/action_basics/src/main/java/icons/ActionBasicsIcons.java
+++ code_samples/action_basics/src/main/java/icons/ActionBasicsIcons.java
@@ -3,10 +3,8 @@
 package icons;
 
 import com.intellij.openapi.util.IconLoader;
-
 import javax.swing.*;
 
 public class ActionBasicsIcons {
-public static final Icon Sdk_default_icon = IconLoader.getIcon("/icons/sdk_16.svg");
-
+  public static final Icon Sdk_default_icon = IconLoader.getIcon("/icons/sdk_16.svg");
 }

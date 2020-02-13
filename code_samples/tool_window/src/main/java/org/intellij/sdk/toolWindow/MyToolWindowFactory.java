beeb04537324031371b6566b9be9120a2fe7c1ee
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2020-02-13 17:20:52 +0100

    IJSDK-787 Convert code sample "tool_window" to use Gradle

---
+++ code_samples/tool_window/src/main/java/org/intellij/sdk/toolWindow/MyToolWindowFactory.java
@@ -0,0 +1,20 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.intellij.sdk.toolWindow;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class MyToolWindowFactory implements ToolWindowFactory {
+  // Create the tool window content.
+  public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+    MyToolWindow myToolWindow = new MyToolWindow(toolWindow);
+    ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+    Content content = contentFactory.createContent(myToolWindow.getContent(), "", false);
+    toolWindow.getContentManager().addContent(content);
+  }
+}

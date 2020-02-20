bb867917412d7dfb5201a5c2a631669e1ae3bf13
JohnHake <john.hake@jetbrains.com>
2020-02-19 21:51:48 -0800

    New Copyright in code samples

--- code_samples/psi_demo/src/main/java/com/intellij/sdk/psi/PsiNavigationDemoAction.java
+++ code_samples/psi_demo/src/main/java/com/intellij/sdk/psi/PsiNavigationDemoAction.java
@@ -1,3 +1,5 @@
+// Copyright 2000-2020 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
 package com.intellij.sdk.psi;
 
 import com.intellij.openapi.actionSystem.AnAction;

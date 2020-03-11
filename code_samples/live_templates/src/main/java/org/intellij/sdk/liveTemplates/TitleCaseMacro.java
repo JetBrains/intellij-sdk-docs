a986370288cd938f64dcc9e9becc4effed43752b
JohnHake <john.hake@jetbrains.com>
2020-03-11 16:09:13 -0700

    [live templates] Add custom macro

---
+++ code_samples/live_templates/src/main/java/org/intellij/sdk/liveTemplates/TitleCaseMacro.java
@@ -0,0 +1,41 @@
+// Copyright 2000-2020 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.intellij.sdk.liveTemplates;
+
+import com.intellij.codeInsight.template.*;
+import com.intellij.codeInsight.template.macro.MacroBase;
+import com.intellij.openapi.util.text.StringUtil;
+import org.jetbrains.annotations.NotNull;
+
+public class TitleCaseMacro extends MacroBase {
+
+  public TitleCaseMacro() {
+    super("TitleCase", "TitleCase(String)");
+  }
+
+  /**
+   * Strictly to uphold contract for constructors in base class.
+   */
+  private TitleCaseMacro(String name, String description) {
+    super(name, description);
+  }
+
+  @Override
+  protected Result calculateResult(@NotNull Expression[] params, ExpressionContext context, boolean quick) {
+    String text = getTextResult(params, context, true);
+    if (text != null) {
+      if (text.length() > 0) {
+        text = StringUtil.toTitleCase(text);
+      }
+      return new TextResult(text);
+    }
+    return null;
+  }
+
+  @Override
+  public boolean isAcceptableInContext(TemplateContextType context) {
+    // Might want to be less restrictive in future
+    return (context instanceof MarkdownContext);
+  }
+
+}

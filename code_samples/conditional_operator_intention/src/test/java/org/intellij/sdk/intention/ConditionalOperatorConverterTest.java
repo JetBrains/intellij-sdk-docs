4b773b7fe448ad429a54d82babf217dab04f4b8f
Yann Cébron <yann.cebron@jetbrains.com>
2020-04-16 11:45:54 +0200

    [code samples] ConditionalOperatorConverterTest: cleanup

--- code_samples/conditional_operator_intention/src/test/java/org/intellij/sdk/intention/ConditionalOperatorConverterTest.java
+++ code_samples/conditional_operator_intention/src/test/java/org/intellij/sdk/intention/ConditionalOperatorConverterTest.java
@@ -1,22 +1,9 @@
 // Copyright 2000-2020 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
 package org.intellij.sdk.intention;
 
 import com.intellij.codeInsight.intention.IntentionAction;
-import com.intellij.openapi.application.PathManager;
-import com.intellij.testFramework.UsefulTestCase;
-import com.intellij.testFramework.builders.JavaModuleFixtureBuilder;
-import com.intellij.testFramework.fixtures.*;
-import org.junit.*;
-
-/**
- * Created by IntelliJ IDEA.
- * User: Alexey.Chursin
- * Date: Sep 13, 2010
- * Time: 9:35:50 PM
- * To change this template use File | Settings | File Templates.
- * This test requires Editor > Code Style set to Default IDE
- */
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+import org.junit.Assert;
 
 public class ConditionalOperatorConverterTest extends LightJavaCodeInsightFixtureTestCase {
 
@@ -32,7 +19,6 @@ public class ConditionalOperatorConverterTest extends LightJavaCodeInsightFixtur
   }
 
   protected void doTest(String testName, String hint) {
-    // Messages.showInfoMessage("Test started", "Info");
     myFixture.configureByFile(testName + ".java");
     final IntentionAction action = myFixture.findSingleIntention(hint);
     Assert.assertNotNull(action);
@@ -40,8 +26,7 @@ public class ConditionalOperatorConverterTest extends LightJavaCodeInsightFixtur
     myFixture.checkResultByFile(testName + ".after.java");
   }
 
-  @Test
-  public void test() {
+  public void testIntention() {
     doTest("before.template", "SDK Convert ternary operator to if statement");
   }
 

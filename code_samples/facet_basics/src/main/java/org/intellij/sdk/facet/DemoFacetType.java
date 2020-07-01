5ed83558e0c8f288c97b8b77e1fdd95650017f4e
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2020-07-01 09:47:39 +0200

    Fixed DemoFacetType imports

--- code_samples/facet_basics/src/main/java/org/intellij/sdk/facet/DemoFacetType.java
+++ code_samples/facet_basics/src/main/java/org/intellij/sdk/facet/DemoFacetType.java
@@ -2,10 +2,14 @@
 
 package org.intellij.sdk.facet;
 
-import com.intellij.facet.*;
-import com.intellij.openapi.module.*;
+import com.intellij.facet.Facet;
+import com.intellij.facet.FacetType;
+import com.intellij.facet.FacetTypeId;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleType;
 import icons.SdkIcons;
-import org.jetbrains.annotations.*;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 

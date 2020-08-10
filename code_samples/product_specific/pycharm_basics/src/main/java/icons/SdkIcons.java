a6ed4eccd9bf6e18fb18708d8b7a6935cf38e4be
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2020-08-10 13:03:26 +0200

    Code Samples: Update Code Style, reformat code

--- code_samples/product_specific/pycharm_basics/src/main/java/icons/SdkIcons.java
+++ code_samples/product_specific/pycharm_basics/src/main/java/icons/SdkIcons.java
@@ -4,6 +4,8 @@ package icons;
 
 import com.intellij.openapi.util.IconLoader;
 
+import javax.swing.*;
+
 public class SdkIcons {
 
   public static final Icon Sdk_default_icon = IconLoader.getIcon("/icons/sdk_16.svg");

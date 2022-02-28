56725e16cd632216991237dba4a5b21577ae8629
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-02-28 17:02:03 +0100

    android_studio.md: generate Android Studio releases list

---
+++ .github/scripts/android_studio_releases.main.kts
@@ -0,0 +1,72 @@
+#!/usr/bin/env kotlin
+
+@file:DependsOn("org.simpleframework:simple-xml:2.7.1")
+@file:DependsOn("org.json:json:20211205")
+
+import org.json.JSONObject
+import org.simpleframework.xml.Attribute
+import org.simpleframework.xml.ElementList
+import org.simpleframework.xml.Path
+import org.simpleframework.xml.Root
+import org.simpleframework.xml.core.Persister
+import java.io.File
+import java.net.URL
+
+val DATA_SERVICES_RELEASES_URL = "https://data.services.jetbrains.com/products/releases"
+val ANDROID_STUDIO_RELEASES_URL = "https://dl.google.com/android/studio/patches/updates.xml"
+val RELEASES_FILE_PATH = "topics/_generated/android_studio_releases.md"
+
+fun resolveMarketingRelease(build: String) = URL("$DATA_SERVICES_RELEASES_URL?code=IC&build=$build").openStream().use {
+  it.readBytes().toString(Charsets.UTF_8).let { content ->
+    (JSONObject(content).getJSONArray("IIC").first() as JSONObject).getString("version")
+  }
+}
+
+URL(ANDROID_STUDIO_RELEASES_URL).openStream().use { inputStream ->
+  inputStream.reader().run {
+    Persister().read(ProductsReleases::class.java, readText())
+  }.run {
+    """
+    | Android Studio | Channel | Build Number | IntelliJ IDEA Build Number | IntelliJ IDEA Release |
+    |----------------|---------|--------------|----------------------------|-----------------------|
+
+    """.trimIndent() + channels.distinctBy { it.number }.joinToString("\n") {
+      val name = it.version.replace('|', '-')
+      val channel = it.status
+      val number = it.number.split('-').last()
+      val ijBuild = it.apiVersion.split('-').last()
+      val ijRelease = ijBuild.let(::resolveMarketingRelease)
+
+      "| $name | $channel | $number | $ijBuild | $ijRelease |"
+    }
+  }.let {
+    "<chunk id=\"releases_table\">\n\n$it\n\n</chunk>"
+  }.let {
+    File(System.getenv("GITHUB_WORKSPACE")).resolve(RELEASES_FILE_PATH).writeText(it)
+  }
+}
+
+@Root(name = "products", strict = false)
+data class ProductsReleases(
+        @field:ElementList(name = "channel", inline = true)
+        @field:Path("product")
+        var channels: List<Channel> = mutableListOf()
+)
+
+@Root(strict = false)
+data class Channel(
+        @field:Attribute
+        var status: String = "",
+
+        @field:Path("build")
+        @field:Attribute
+        var apiVersion: String = "",
+
+        @field:Path("build")
+        @field:Attribute
+        var number: String = "",
+
+        @field:Path("build")
+        @field:Attribute
+        var version: String = "",
+)
---
+++ topics/_generated/android_studio_releases.md
@@ -0,0 +1,9 @@
+<chunk id="releases_table">
+
+| Android Studio | Channel | Build Number | IntelliJ IDEA Build Number | IntelliJ IDEA Release |
+|----------------|---------|--------------|----------------------------|-----------------------|
+| Dolphin - 2021.3.1 Canary 3 | eap | 213.6777.52.2113.8172706 | 213.6777.52 | 2021.3.2 |
+| Chipmunk - 2021.2.1 Beta 2 | beta | 212.5712.43.2112.8125332 | 212.5712.43 | 2021.2.4 |
+| Bumblebee - 2021.1.1 Patch 2 | release | 211.7628.21.2111.8193401 | 211.7628.21 | 2021.1.3 |
+
+</chunk>

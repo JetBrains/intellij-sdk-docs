f4c5afc2a0dd18d4b117274e4c1714c0bd2663a0
Karol Lewandowski <karol86@gmail.com>
2022-03-23 16:10:12 +0100

    migrate from groovy to kotlin gradle build scripts in the code samples

---
+++ code_samples/product_specific/pycharm_basics/build.gradle.kts
@@ -0,0 +1,37 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+plugins {
+  id("java")
+  id("org.jetbrains.intellij") version "1.4.0"
+}
+
+group = "com.intellij.sdk"
+version = "0.1.0"
+
+repositories {
+  mavenCentral()
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_11
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+  version.set("2021.2")
+  type.set("PY")
+  plugins.set(listOf("Pythonid"))
+  downloadSources.set(false)
+}
+
+tasks {
+  buildSearchableOptions {
+    enabled = false
+  }
+
+  patchPluginXml {
+    version.set("${project.version}")
+    sinceBuild.set("212")
+    untilBuild.set("213.*")
+  }
+}
---
+++ code_samples/run_configuration/build.gradle.kts
@@ -0,0 +1,34 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+plugins {
+  id("java")
+  id("org.jetbrains.intellij") version "1.4.0"
+}
+
+group = "com.intellij.sdk"
+version = "2.0.0"
+
+repositories {
+  mavenCentral()
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_11
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+  version.set("2021.1.3")
+}
+
+tasks {
+  buildSearchableOptions {
+    enabled = false
+  }
+
+  patchPluginXml {
+    version.set("${project.version}")
+    sinceBuild.set("211")
+    untilBuild.set("213.*")
+  }
+}

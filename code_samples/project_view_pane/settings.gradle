c9d2e683931d2dd49f7315ff4a0f6b15d71dcb83
Karol Lewandowski <karol86@gmail.com>
2022-03-23 14:46:06 +0100

    settings.gradle (all): update copyright header

--- code_samples/project_view_pane/settings.gradle
+++ code_samples/project_view_pane/settings.gradle
@@ -1,3 +1,3 @@
-// Copyright 2000-2022 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 rootProject.name = 'project_view_pane'

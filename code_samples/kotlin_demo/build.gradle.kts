0e0cede9d44c8150276770d27b4fe3855258c95f
Yann Cébron <yann.cebron@jetbrains.com>
2023-04-25 09:38:52 +0200

    code sample: kotlin 1.8.21

--- code_samples/kotlin_demo/build.gradle.kts
+++ code_samples/kotlin_demo/build.gradle.kts
@@ -3,7 +3,7 @@
 plugins {
   id("java")
   id("org.jetbrains.intellij") version "1.13.3"
-  id("org.jetbrains.kotlin.jvm") version "1.8.20"
+  id("org.jetbrains.kotlin.jvm") version "1.8.21"
 }
 
 group = "org.intellij.sdk"

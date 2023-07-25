fbd81187afaf9f90c2297b9a2035b778ac9f2a13
Yann Cébron <yann.cebron@jetbrains.com>
2023-07-25 10:33:09 +0200

    code samples/psi_demo: cleanup PsiNavigationDemoAction

--- code_samples/psi_demo/src/main/java/org/intellij/sdk/psi/PsiNavigationDemoAction.java
+++ code_samples/psi_demo/src/main/java/org/intellij/sdk/psi/PsiNavigationDemoAction.java
@@ -1,7 +1,8 @@
-// Copyright 2000-2022 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.intellij.sdk.psi;
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
@@ -9,16 +10,22 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.psi.*;
 import com.intellij.psi.util.PsiTreeUtil;
+import org.jetbrains.annotations.NotNull;
 
 public class PsiNavigationDemoAction extends AnAction {
+
+  @Override
+  public @NotNull ActionUpdateThread getActionUpdateThread() {
+    return ActionUpdateThread.BGT;
+  }
 
   @Override
   public void actionPerformed(AnActionEvent anActionEvent) {
     Editor editor = anActionEvent.getData(CommonDataKeys.EDITOR);
     PsiFile psiFile = anActionEvent.getData(CommonDataKeys.PSI_FILE);
-      if (editor == null || psiFile == null) {
-          return;
-      }
+    if (editor == null || psiFile == null) {
+      return;
+    }
     int offset = editor.getCaretModel().getOffset();
 
     final StringBuilder infoBuilder = new StringBuilder();
@@ -40,7 +47,7 @@ public class PsiNavigationDemoAction extends AnAction {
         infoBuilder.append("Local variables:\n");
         containingMethod.accept(new JavaRecursiveElementVisitor() {
           @Override
-          public void visitLocalVariable(PsiLocalVariable variable) {
+          public void visitLocalVariable(@NotNull PsiLocalVariable variable) {
             super.visitLocalVariable(variable);
             infoBuilder.append(variable.getName()).append("\n");
           }

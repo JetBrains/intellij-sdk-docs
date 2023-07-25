f074bf884109150188045ebc74582340d202c96f
Yann Cébron <yann.cebron@jetbrains.com>
2023-07-25 16:21:45 +0200

    code samples/live_templates: cleanup

--- code_samples/live_templates/src/main/java/org/intellij/sdk/liveTemplates/MarkdownContext.java
+++ code_samples/live_templates/src/main/java/org/intellij/sdk/liveTemplates/MarkdownContext.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.intellij.sdk.liveTemplates;
 
@@ -9,7 +9,7 @@ import org.jetbrains.annotations.NotNull;
 public class MarkdownContext extends TemplateContextType {
 
   protected MarkdownContext() {
-    super("MARKDOWN", "Markdown");
+    super("Markdown");
   }
 
   @Override

89e5c41674d5df0da5c23234b20272051699263d
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2024-03-15 17:41:24 +0100

    Update target version and since/until in Gradle build scripts

--- code_samples/conditional_operator_intention/build.gradle.kts
+++ code_samples/conditional_operator_intention/build.gradle.kts
@@ -22,7 +22,7 @@ java {
 
 // See https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2023.1.5")
+  version.set("2023.2.6")
   plugins.set(listOf("com.intellij.java"))
 }
 
@@ -33,7 +33,7 @@ tasks {
 
   patchPluginXml {
     version.set("${project.version}")
-    sinceBuild.set("231")
-    untilBuild.set("233.*")
+    sinceBuild.set("232")
+    untilBuild.set("241.*")
   }
 }

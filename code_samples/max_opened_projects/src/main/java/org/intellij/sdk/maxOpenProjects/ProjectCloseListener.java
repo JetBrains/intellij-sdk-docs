c2aa0179524d918176b200314bca4ff643e78881
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2024-03-15 18:08:15 +0100

    Migrate StartupActivity.DumbAware to ProjectActivity

--- code_samples/max_opened_projects/src/main/java/org/intellij/sdk/maxOpenProjects/ProjectCloseListener.java
+++ code_samples/max_opened_projects/src/main/java/org/intellij/sdk/maxOpenProjects/ProjectCloseListener.java
@@ -1,7 +1,8 @@
-// Copyright 2000-2022 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.intellij.sdk.maxOpenProjects;
 
+import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManagerListener;
@@ -15,13 +16,13 @@ public class ProjectCloseListener implements ProjectManagerListener {
   @Override
   public void projectClosed(@NotNull Project project) {
     // Ensure this isn't part of testing
-    if (ApplicationManager.getApplication().isUnitTestMode()) {
+    Application application = ApplicationManager.getApplication();
+    if (application.isUnitTestMode()) {
       return;
     }
 
     // Get the counting service
-    ProjectCountingService projectCountingService =
-        ApplicationManager.getApplication().getService(ProjectCountingService.class);
+    ProjectCountingService projectCountingService = application.getService(ProjectCountingService.class);
     // Decrement the count because a project just closed
     projectCountingService.decreaseOpenProjectCount();
   }

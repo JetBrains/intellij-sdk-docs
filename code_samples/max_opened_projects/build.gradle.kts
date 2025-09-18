b94be711f586f6686048c576f21092365fc17b4a
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2025-09-18 15:44:16 +0200

    Update `intellijPlatform` and `sinceBuild` in code samples
    We intentionally didn't switch from IC to IU (from 2025.3, there will be one distribution: IU). We should switch once we target 2025.3.

--- code_samples/max_opened_projects/build.gradle.kts
+++ code_samples/max_opened_projects/build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 
 dependencies {
   intellijPlatform {
-    intellijIdeaCommunity("2024.3.6")
+    intellijIdeaCommunity("2025.1.5.1")
   }
 }
 
@@ -30,7 +30,7 @@ intellijPlatform {
 
   pluginConfiguration {
     ideaVersion {
-      sinceBuild = "243"
+      sinceBuild = "251"
     }
   }
   pluginVerification  {
--- code_samples/simple_language_plugin/build.gradle.kts
+++ code_samples/simple_language_plugin/build.gradle.kts
@@ -30,7 +30,7 @@ repositories {
 
 dependencies {
   intellijPlatform {
-    intellijIdeaCommunity("2024.3.6")
+    intellijIdeaCommunity("2025.1.5.1")
     bundledPlugin("com.intellij.java")
 
     testFramework(TestFrameworkType.Plugin.Java)
@@ -47,7 +47,7 @@ intellijPlatform {
 
   pluginConfiguration {
     ideaVersion {
-      sinceBuild = "243"
+      sinceBuild = "251"
     }
   }
   pluginVerification  {

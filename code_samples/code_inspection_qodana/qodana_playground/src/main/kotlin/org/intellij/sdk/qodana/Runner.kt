9d6c7b244dd06e90071be8879b46bd75bcb4471d
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2025-10-28 10:07:59 +0100

    Add copyright notices

--- code_samples/code_inspection_qodana/qodana_playground/src/main/kotlin/org/intellij/sdk/qodana/Runner.kt
+++ code_samples/code_inspection_qodana/qodana_playground/src/main/kotlin/org/intellij/sdk/qodana/Runner.kt
@@ -1,3 +1,5 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
 package org.intellij.sdk.qodana
 
 import org.intellij.sdk.qodana.service.AnotherComponent

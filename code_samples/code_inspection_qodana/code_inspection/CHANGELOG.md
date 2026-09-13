bd7d618a6664e2421676c820fcb77c1db4a072a9
Robert Novotny <robert.novotny@jetbrains.com>
2026-09-13 23:26:50 +0200

    Remove changelog configuration
    Changelog is empty and unnecessary for this example

--- code_samples/code_inspection_qodana/code_inspection/CHANGELOG.md
+++
@@ -1,9 +0,0 @@
-<!-- Keep a Changelog guide -> https://keepachangelog.com -->
-
-# SDK: Comparing References Inspection Sample
-
-## [Unreleased]
-
-### Added
-
-- Initial release
--- code_samples/code_inspection_qodana/code_inspection/settings.gradle.kts
+++ code_samples/code_inspection_qodana/code_inspection/settings.gradle.kts
@@ -6,7 +6,6 @@ rootProject.name = "code_inspection"
 pluginManagement {
   plugins {
     id("org.jetbrains.kotlin.jvm") version "2.1.20"
-    id("org.jetbrains.changelog") version "2.5.0"
     id("org.jetbrains.qodana") version "2026.1.3"
   }
 }

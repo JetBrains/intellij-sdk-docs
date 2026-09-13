53d0390ab9c0feb2e5d5c34426feaf3b3649a265
Robert Novotny <robert.novotny@jetbrains.com>
2026-09-14 00:44:10 +0200

    Remove unit test setup
    Unit tests are not used

--- code_samples/code_inspection_qodana/qodana_playground/build.gradle.kts
+++ code_samples/code_inspection_qodana/qodana_playground/build.gradle.kts
@@ -1,4 +1,4 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 plugins {
   id("org.jetbrains.kotlin.jvm") version "2.2.0"
@@ -9,14 +9,6 @@ version = "1.0-SNAPSHOT"
 
 repositories {
   mavenCentral()
-}
-
-dependencies {
-  testImplementation(kotlin("test"))
-}
-
-tasks.test {
-  useJUnitPlatform()
 }
 
 kotlin {

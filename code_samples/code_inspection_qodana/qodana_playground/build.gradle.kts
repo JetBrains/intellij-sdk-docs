a53f561be1fc86ceeba97ce22e1637ef1016b778
Robert Novotny <robert.novotny@jetbrains.com>
2026-09-14 00:44:57 +0200

    Bump Kotlin in Playground

--- code_samples/code_inspection_qodana/qodana_playground/build.gradle.kts
+++ code_samples/code_inspection_qodana/qodana_playground/build.gradle.kts
@@ -1,7 +1,7 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 plugins {
-  id("org.jetbrains.kotlin.jvm") version "2.2.0"
+  id("org.jetbrains.kotlin.jvm") version "2.3.21"
 }
 
 group = "org.intellij.sdk"

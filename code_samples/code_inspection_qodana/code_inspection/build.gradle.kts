38a48564bacad2cd21295e5647ad3421796cc226
Robert Novotny <robert.novotny@jetbrains.com>
2026-09-14 11:41:06 +0200

    code_inspections_qodana: Freshen up dependencies and setup (#2186)
    * Remove changelog configuration. Changelog is empty and unnecessary for this example
    * Bump Kotlin and IntelliJ Platform version
    * Bump Kotlin version in Playground 
    * Bump Qodana Gradle plugin version
    * Remove unit test setup in Playground. Unit tests are not used

--- code_samples/code_inspection_qodana/code_inspection/build.gradle.kts
+++ code_samples/code_inspection_qodana/code_inspection/build.gradle.kts
@@ -1,7 +1,6 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 plugins {
-  id("java")
   id("org.jetbrains.kotlin.jvm")
   id("org.jetbrains.intellij.platform")
   id("org.jetbrains.qodana")
@@ -9,7 +8,7 @@ plugins {
 
 dependencies {
   intellijPlatform {
-    intellijIdeaCommunity("2024.3.6")
+    intellijIdea("2025.3.6.1")
     bundledPlugin("org.jetbrains.kotlin")
   }
 }
